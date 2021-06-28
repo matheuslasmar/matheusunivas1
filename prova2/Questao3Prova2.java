40b7d6e469c2d20d46cce15d31bcb7070a70a170
matheuslasmar <81999737+matheuslasmar@users.noreply.github.com>
2021-06-28 19:22:14 -0300

    Update Questao3Prova2.java

--- prova2/Questao3Prova2.java
+++ prova2/Questao3Prova2.java
@@ -1,12 +1,12 @@
 package br.edu.univas.prova;
 import java.util.Scanner;
 
-public class Questao3Prova2 {
+public class Questao1Prova2 {
 
 	public static void main(String[] args) {
 		Scanner leitura = new Scanner(System.in);
 		
-		int[] notas = new int [10];
+		int[] notas = new int [50];
 		int direto = 0;
 		int recup = 0;
 		int depend = 0;
@@ -14,7 +14,7 @@ public class Questao3Prova2 {
 		int porcentagemrecup = 0;
 		int porcentagemdepend = 0;
 		
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 50; i++) {
 			System.out.println("Digite a nota");
 			notas[i] = leitura.nextInt();
 			
@@ -29,15 +29,15 @@ public class Questao3Prova2 {
 			}
 		}
 		
-		porcentagemdireto = (direto * 100) / (10);
-		porcentagemrecup = (recup * 100) / (10);
-		porcentagemdepend = (depend * 100) / (10);
+		porcentagemdireto = (direto * 100) / (50);
+		porcentagemrecup = (recup * 100) / (50);
+		porcentagemdepend = (depend * 100) / (50);
 
 		System.out.println("Quantidade de alunos que passaram direto: "+ direto);
 		System.out.println("Porcentagem : " +porcentagemdireto+ "%");
-		System.out.println("Quantidade de alunos que estão em recuperação: "+ recup);
+		System.out.println("Quantidade de alunos que estÃ£o em recuperaÃ§Ã£o: "+ recup);
 		System.out.println("Porcentagem : " +porcentagemrecup+ "%");
-		System.out.println("Quantidade de alunos que estão em dependência: "+ depend);
+		System.out.println("Quantidade de alunos que estÃ£o em dependÃªncia: "+ depend);
 		System.out.println("Porcentagem : " +porcentagemdepend+ "%");
 		
 		
